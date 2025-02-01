81376e68c5ffd943f4abe96cc21f71765f40c026
Ben L. <ben.leiby25@gmail.com>
2025-02-01 14:31:34 -0500

    Added rest apiview

--- backend/apiserver/apiserver/settings.py
+++ backend/apiserver/apiserver/settings.py
@@ -41,8 +41,18 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'rest_framework',
     'allauth.socialaccount.providers.google', # google provider for auth
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- backend/apiserver/frontreq/views.py
+++ backend/apiserver/frontreq/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status  # Import status codes
 
 # Create your views here.
 
-def reverse_string(Request):
-    return HttpResponse("Hello from django!")
+@api_view(['GET'])  # Only allow GET requests
+def reverse_string(request):
+    my_string = "Hello from my Django!"  # The string you want to return
+    return Response(my_string, status=status.HTTP_200_OK, content_type='text/plain') # important to add content type.
